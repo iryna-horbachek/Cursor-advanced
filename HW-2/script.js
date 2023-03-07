e2f11f1716f1dffabf34b654953443a119739f41
Iryna Horbachek <ira.gorbachek@gmail.com>
2023-03-07 21:03:53 +0200

    changed script.js

--- HW-2/script.js
+++ HW-2/script.js
@@ -5,23 +5,30 @@ let result = 0;
 
 if (isNaN(numberOne) || isNaN(numberTwo)) {
     alert('Це не число');
-} else if (numberOne <= 0 || numberTwo <= 0) {
+}
+else if (!Number.isInteger(numberOne)|| !Number.isInteger(numberTwo)) {
+    alert('Введіть ціле число')
+} 
+else if (numberOne <= 0 || numberTwo <= 0) {
     alert('Ведіть число більше 0')
-} else if (numberTwo <= numberOne) {
+}
+else if (numberTwo <= numberOne) {
     alert('Друге число має бути більшим ніж перше')
 }
 else {
-   console.log(parseInt(numberOne));
-   console.log(parseInt(numberTwo));
-}
-
-for(let i = numberOne; i <= numberTwo; i++) {
+   console.log(numberOne);
+   console.log(numberTwo);
+   for(let i = numberOne; i <= numberTwo; i++) {
     if (!addEvenNumber) {
         if (i % 2 === 0) {
             continue;
             }
-        }         
-        result += i;
-    }
-    console.log(parseInt(result));
+        }    
+            result += i; 
+}
+console.log(result);
+   
+}
+
+
    
