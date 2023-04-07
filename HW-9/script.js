171e7660614f71ebd94ea3c12d8d0b1c811b54ec
Iryna Horbachek <ira.gorbachek@gmail.com>
2023-04-07 10:24:12 +0300

    added HW-9

---
+++ HW-9/script.js
@@ -0,0 +1,25 @@
+function generateBlocks() {
+    const container = document.querySelector('.div');
+    const length = 25;
+    container.innerHTML = '';
+        for (let i = 0; i < length; i++) {
+            const square = document.createElement('square');
+            square.style.background = getRandomColor();
+            container.append(square);
+        }              
+}
+
+function getRandomColor() {
+    const firstColor = Math.floor(Math. random() * 256);
+    const secondColor = Math.floor(Math. random() * 256);
+    const thirdColor = Math.floor(Math. random() * 256);
+    const randomColor = `rgb(${firstColor}, ${secondColor}, ${thirdColor})`;
+    return randomColor;
+}
+
+function generateBlocksInterval() {
+    const interval = setInterval(generateBlocks, 1000);
+    return interval;
+}
+
+generateBlocksInterval();
