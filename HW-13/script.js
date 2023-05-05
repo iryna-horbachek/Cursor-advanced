8882fef9cb3c77b5919b30278fe59f12e1ef99a0
Iryna Horbachek <ira.gorbachek@gmail.com>
2023-05-05 13:56:26 +0300

    added HW-13

---
+++ HW-13/script.js
@@ -0,0 +1,41 @@
+// 1
+function* createIdGenerator() {
+    for (let i = 1; i < Infinity; i++) {
+        yield i;
+    }
+}
+
+const idGenerator = createIdGenerator();
+
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+console.log(idGenerator.next().value);
+
+//2
+const upBtn = document.getElementById('up');
+const downBtn = document.getElementById('down');
+const currentText = document.getElementById('text');
+
+function* newFontGenerator(initFontSize) {
+    let fontValue = initFontSize;
+    for (let i = 0; i < Infinity; i++) {
+        const initValue = yield;
+        if (initValue === 'up') {
+            fontValue += 2;
+            currentText.style.fontSize = fontValue + 'px';
+        } else if (initValue === 'down') {
+            fontValue -= 2;
+            currentText.style.fontSize = fontValue + 'px';
+        }
+    }
+}
+
+const fontGenerator = newFontGenerator(14);
+
+upBtn.addEventListener("click", () => {
+    fontGenerator.next('up').value;
+})
+
+downBtn.addEventListener("click", () => {
+    fontGenerator.next('down').value;
+})
